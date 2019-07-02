6025f93c19d45cebad79c52239accc7b1ea6914d
Jake Brose <mancinijake@gmail.com>
2019-07-02 07:09:30 -0500

    added click functionality

--- src/index.js
+++ src/index.js
@@ -2,19 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-            {/* TODO */}
+function Square(props) {
+    return (
+        <button 
+            className="square" 
+            onClick={props.onClick}
+        >
+            {props.value}
         </button>
-        );
-    }
+    );
 }
 
+
+
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array().fill(null),
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)} 
+            />
+        );
     }
 
     render() {
@@ -58,6 +80,7 @@ class Game extends React.Component {
         );
     }
 }
+
 
 // ===============================================
 
